c9b8341275efe4f4d787abbf5951695da7b6188f
W2GOfficial <wesleya.isotton@universo.univates.br>
2019-10-07 15:44:34 -0300

    routes and packages

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Routes from './src/Routes';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return <Routes />;
 }
 
 const styles = StyleSheet.create({
